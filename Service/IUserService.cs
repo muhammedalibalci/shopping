d5cdef94962d3b200fbe1a7655d4efe7f783fc21
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-06-04 21:18:29 +0300

    First try

---
+++ Service/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IUserService
+    {
+        Task<BaseResponseDto<UserDto>> GetAsync(int id);
+    }
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Dto;
+using Domain.Interfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+
+            _userRepository = userRepository;
+        }
+
+        public async Task<BaseResponseDto<UserDto>> GetAsync(int id)
+        {
+            BaseResponseDto<UserDto> getUserResponse = new BaseResponseDto<UserDto>();
+
+           
+                User user = await _userRepository.GetAsync(id);
+
+                if (user != null)
+                {
+                    getUserResponse.Data = new UserDto
+                    {
+                        Name = user.FirstName
+                    };
+                }
+                else
+                {
+                    getUserResponse.Errors.Add("User not found.");
+                }
+           
+
+            return getUserResponse;
+        }
+    }
+}
