c2d75ca611422999c0ac9d4b9f421491dce8bf7d
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-06-13 19:34:23 +0300

    fix file

---
+++ Service/Helpers/FileConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Service.Helpers
+{
+    public static class FileConfiguration
+    {
+        public static bool FileUpload(IFormFile file)
+        {
+            var folderName = Path.Combine("Resources", "Images");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+            if (file.Length > 0)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                var fullPath = Path.Combine(pathToSave, fileName);
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return true;
+            }
+            return false;
+        } 
+    }
+}
