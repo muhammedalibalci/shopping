e79a5148658a351ce4a67c7511bee0cc25376ff1
Muhammed Ali <40212997+muhammedalibalci@users.noreply.github.com>
2020-06-13 19:47:08 +0300

    Create CategoriesController.cs

---
+++ UI/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Service.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> List()
+        {
+            return await _categoryService.GetAll();
+        }
+    }
+}
